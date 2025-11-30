0e55397c0a5d143023229bb60dad8148adb2c786
Adithya Suresh <adithyasuresh201@gmail.com>
2025-11-30 21:38:47 +0530

    add appbar & leading widgets + move example pages to presentation

--- example/lib/presentation/home/view.dart
+++ example/lib/presentation/home/view.dart
@@ -11,22 +11,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: .center,
+      appBar: AppBar(title: Text('Sliding Panel Kit Examples')),
+      body: ListView(
         children: [
           ListTile(
+            leading: Text('1.'),
             title: Text('Basic Usage'),
             onTap: () {
               // context.go();
             },
           ),
           ListTile(
+            leading: Text('2.'),
             title: Text('Custom Snapping'),
             onTap: () {
               // context.go();
             },
           ),
           ListTile(
+            leading: Text('3.'),
             title: Text('Parallax'),
             onTap: () {
               // context.go();
