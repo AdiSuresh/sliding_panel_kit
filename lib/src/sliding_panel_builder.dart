0420cdead5a3ccd21aff29a8fc1fe2bec716e94d
Adithya Suresh <adithyasuresh201@gmail.com>
2025-12-17 19:07:18 +0530

    lock extent during horizontal scrolling

--- lib/src/sliding_panel_builder.dart
+++ lib/src/sliding_panel_builder.dart
@@ -241,7 +241,7 @@ final class _SlidingPanelBuilderState extends State<SlidingPanelBuilder>
       onNotification: (notification) {
         final ScrollNotification(:metrics, :context) = notification;
 
-        if (metrics.axis == Axis.horizontal || context == null) {
+        if (metrics.axis == .vertical || context == null) {
           return false;
         }
 
@@ -251,147 +251,171 @@ final class _SlidingPanelBuilderState extends State<SlidingPanelBuilder>
             pointerTracker.exceed();
             break;
 
-          case ScrollUpdateNotification(:final dragDetails):
-            final position = Scrollable.of(context).position;
-            final canScroll = [
-              widget.minExtent,
-              snapPoint,
-              widget.maxExtent,
-            ].contains(controller.value);
-
-            if (dragDetails == null) {
-              if (!canScroll) {
-                position.correctBy(-(notification.scrollDelta ?? 0.0));
-                position.hold(() {}).cancel();
-              }
-              break;
-            }
-
-            final dy = dragDetails.delta.dy;
-
-            final ScrollPosition(
-              :outOfRange,
-              :axisDirection,
-              :pixels,
-              :minScrollExtent,
-              :maxScrollExtent,
-              :correctPixels,
-              :correctBy,
-            ) = position;
-
-            if (outOfRange) {
-              final correction = dy.abs();
-              if (pixels < minScrollExtent) {
-                if (pixels + correction >= minScrollExtent) {
-                  if (!canScroll) {
-                    correctPixels(minScrollExtent);
-                  }
-                  scrollAreaTracker.reset();
-                  break;
-                }
-                if (!canScroll) {
-                  correctBy(correction);
-                  scrollAreaTracker.reset();
-                  break;
-                }
-              } else {
-                if (pixels - correction <= maxScrollExtent) {
-                  if (!canScroll) {
-                    correctPixels(maxScrollExtent);
-                  }
-                  scrollAreaTracker.reset();
-                  break;
-                }
-                if (!canScroll) {
-                  correctBy(-correction);
-                  scrollAreaTracker.reset();
-                  break;
-                }
-              }
-            } else if (!canScroll) {
-              final correction = !axisDirection.reverse ? dy : -dy;
-              final newPixels = pixels + correction;
-              if (newPixels < minScrollExtent) {
-                correctPixels(minScrollExtent);
-              } else if (newPixels > maxScrollExtent) {
-                correctPixels(maxScrollExtent);
-              } else {
-                correctBy(correction);
-              }
-              scrollAreaTracker.reset();
-              break;
-            }
-
-            scrollAreaTracker.update(context.findRect());
-            break;
-
-          case UserScrollNotification(direction: != .idle):
-            break;
-
-          case OverscrollNotification():
-            scrollAreaTracker.reset();
-            break;
-
-          case _:
+          case UserScrollNotification(direction: .idle) ||
+              ScrollEndNotification():
             scrollAreaTracker.reset();
             pointerTracker.reset();
+            break;
         }
 
         return false;
       },
-      child: Listener(
-        onPointerDown: (event) {
-          pointerTracker.start(context, event);
-          velocityTracker = VelocityTracker.withKind(event.kind);
-          drag(event.delta.dy);
-        },
-        onPointerMove: (event) {
-          if (scrollAreaTracker.contains(event.position)) {
-            return;
+      child: NotificationListener<ScrollNotification>(
+        onNotification: (notification) {
+          final ScrollNotification(:metrics, :context) = notification;
+
+          if (metrics.axis == .horizontal || context == null) {
+            return false;
           }
 
-          velocityTracker?.addPosition(event.timeStamp, event.position);
+          switch (notification) {
+            case ScrollStartNotification():
+              scrollAreaTracker.update(context.findRect());
+              pointerTracker.exceed();
+              break;
 
-          if (!pointerTracker.update(viewId, event)) {
-            return;
+            case ScrollUpdateNotification(:final dragDetails):
+              final position = Scrollable.of(context).position;
+              final canScroll = [
+                widget.minExtent,
+                snapPoint,
+                widget.maxExtent,
+              ].contains(controller.value);
+
+              if (dragDetails == null) {
+                if (!canScroll) {
+                  position.correctBy(-(notification.scrollDelta ?? 0.0));
+                  position.hold(() {}).cancel();
+                }
+                break;
+              }
+
+              final dy = dragDetails.delta.dy;
+
+              final ScrollPosition(
+                :outOfRange,
+                :axisDirection,
+                :pixels,
+                :minScrollExtent,
+                :maxScrollExtent,
+                :correctPixels,
+                :correctBy,
+              ) = position;
+
+              if (outOfRange) {
+                final correction = dy.abs();
+                if (pixels < minScrollExtent) {
+                  if (pixels + correction >= minScrollExtent) {
+                    if (!canScroll) {
+                      correctPixels(minScrollExtent);
+                    }
+                    scrollAreaTracker.reset();
+                    break;
+                  }
+                  if (!canScroll) {
+                    correctBy(correction);
+                    scrollAreaTracker.reset();
+                    break;
+                  }
+                } else {
+                  if (pixels - correction <= maxScrollExtent) {
+                    if (!canScroll) {
+                      correctPixels(maxScrollExtent);
+                    }
+                    scrollAreaTracker.reset();
+                    break;
+                  }
+                  if (!canScroll) {
+                    correctBy(-correction);
+                    scrollAreaTracker.reset();
+                    break;
+                  }
+                }
+              } else if (!canScroll) {
+                final correction = !axisDirection.reverse ? dy : -dy;
+                final newPixels = pixels + correction;
+                if (newPixels < minScrollExtent) {
+                  correctPixels(minScrollExtent);
+                } else if (newPixels > maxScrollExtent) {
+                  correctPixels(maxScrollExtent);
+                } else {
+                  correctBy(correction);
+                }
+                scrollAreaTracker.reset();
+                break;
+              }
+
+              scrollAreaTracker.update(context.findRect());
+              break;
+
+            case UserScrollNotification(direction: != .idle):
+              break;
+
+            case OverscrollNotification():
+              scrollAreaTracker.reset();
+              break;
+
+            case _:
+              scrollAreaTracker.reset();
+              pointerTracker.reset();
           }
 
-          drag(event.delta.dy);
+          return false;
         },
-        onPointerUp: (event) {
-          pointerTracker.reset();
-          if (!scrollAreaTracker.contains(event.position)) {
-            snap();
-          }
-          scrollAreaTracker.reset();
-        },
-        child: ValueListenableBuilder<double>(
-          valueListenable: controller,
-          builder: (context, value, child) {
-            return LayoutBuilder(
-              builder: (context, constraints) {
-                final availablePixels = constraints.biggest.height;
-
-                controller._availablePixels = availablePixels;
-
-                final maxHeight = availablePixels * widget.maxExtent;
-                final minHeight = availablePixels * widget.minExtent;
-
-                final travel = (maxHeight - minHeight) - widget._handleHeight;
-
-                final dy = (1 - controller.normalizedValue) * travel;
-
-                return Transform.translate(
-                  offset: Offset(0, dy),
-                  child: ConstrainedBox(
-                    constraints: constraints.copyWith(maxHeight: maxHeight),
-                    child: child,
-                  ),
-                );
-              },
-            );
+        child: Listener(
+          onPointerDown: (event) {
+            pointerTracker.start(context, event);
+            velocityTracker = VelocityTracker.withKind(event.kind);
+            drag(event.delta.dy);
           },
-          child: widget.builder(context, widget.handle),
+          onPointerMove: (event) {
+            if (scrollAreaTracker.contains(event.position)) {
+              return;
+            }
+
+            velocityTracker?.addPosition(event.timeStamp, event.position);
+
+            if (!pointerTracker.update(viewId, event)) {
+              return;
+            }
+
+            drag(event.delta.dy);
+          },
+          onPointerUp: (event) {
+            pointerTracker.reset();
+            if (!scrollAreaTracker.contains(event.position)) {
+              snap();
+            }
+            scrollAreaTracker.reset();
+          },
+          child: ValueListenableBuilder<double>(
+            valueListenable: controller,
+            builder: (context, value, child) {
+              return LayoutBuilder(
+                builder: (context, constraints) {
+                  final availablePixels = constraints.biggest.height;
+
+                  controller._availablePixels = availablePixels;
+
+                  final maxHeight = availablePixels * widget.maxExtent;
+                  final minHeight = availablePixels * widget.minExtent;
+
+                  final travel = (maxHeight - minHeight) - widget._handleHeight;
+
+                  final dy = (1 - controller.normalizedValue) * travel;
+
+                  return Transform.translate(
+                    offset: Offset(0, dy),
+                    child: ConstrainedBox(
+                      constraints: constraints.copyWith(maxHeight: maxHeight),
+                      child: child,
+                    ),
+                  );
+                },
+              );
+            },
+            child: widget.builder(context, widget.handle),
+          ),
         ),
       ),
     );
